c28fb83fda68f6700070157cf7102bb91146cfde
Ryan Kotzen <Ryank@entelect.co.za>
2014-09-30 13:34:07 +0200

    Removing redundant usings.
    Canging points cost to decimal.

--- src/Entelect.Encentivize.Sdk/Rewards/Reward.cs
+++ src/Entelect.Encentivize.Sdk/Rewards/Reward.cs
@@ -5,7 +5,7 @@ namespace Entelect.Encentivize.Sdk.Rewards
         public long RewardId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int PointsCost { get; set; }
+        public decimal PointsCost { get; set; }
         public string TermsAndConditions { get; set; }
         public string PrimaryImageUrl { get; set; }
     }
