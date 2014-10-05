d397b0594f3ba60528b527b9ccb33265c85af277
Sereph <ryank@entelect.co.za>
2014-10-05 19:25:54 +0200

    Adding group role client

---
+++ src/Entelect.Encentivize.Sdk/MemberGrouping/GroupRoles/GroupRolesClient.cs
@@ -0,0 +1,46 @@
+﻿using Entelect.Encentivize.Sdk.GenericServices;
+using RestSharp;
+
+namespace Entelect.Encentivize.Sdk.MemberGrouping.GroupRoles
+{
+    public class GroupRolesClient : IGroupRolesClient
+    {
+        private readonly IEntityUpdateService<GroupRoleInput, GroupRoleOutput> _entityUpdateService;
+        private readonly IEntityRetrievalService<GroupRoleOutput> _entityRetrievalService;
+        private readonly IEntityCreationService<GroupRoleInput, GroupRoleOutput> _entityCreationService;
+        private readonly IEntityDeletionService _entityDeletionService;
+        public GroupRolesClient(IRestClient restClient)
+        {
+            var entitySettings = new EntitySettings("Group Role", "Group Roles", "GroupRoles");
+            _entityUpdateService = new EntityUpdateService<GroupRoleInput, GroupRoleOutput>(restClient, entitySettings);
+            _entityRetrievalService = new EntityRetrievalService<GroupRoleOutput>(restClient, entitySettings);
+            _entityCreationService = new EntityCreationService<GroupRoleInput, GroupRoleOutput>(restClient, entitySettings);
+            _entityDeletionService = new EntityDeletionService(restClient, entitySettings);
+        }
+
+        public GroupRoleOutput Get(long groupRoleId)
+        {
+            return _entityRetrievalService.GetById(groupRoleId);
+        }
+
+        public PagedResult<GroupRoleOutput> Search(GroupRoleSearchCriteria groupRoleSearchCriteria)
+        {
+            return _entityRetrievalService.FindBySearchCriteria(groupRoleSearchCriteria);
+        }
+
+        public GroupRoleOutput Create(GroupRoleInput groupRoleInput)
+        {
+            return _entityCreationService.Create(groupRoleInput);
+        }
+
+        public GroupRoleOutput Update(long groupRoleId, GroupRoleInput groupRoleInput)
+        {
+            return _entityUpdateService.Update(groupRoleId, groupRoleInput);
+        }
+
+        public void Delete(long groupRoleId)
+        {
+            _entityDeletionService.Delete(groupRoleId);
+        }
+    }
+}
---
+++ src/Entelect.Encentivize.Sdk/MemberGrouping/GroupRoles/IGroupRolesClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Entelect.Encentivize.Sdk.MemberGrouping.GroupRoles
+{
+    public interface IGroupRolesClient
+    {
+        GroupRoleOutput Get(long groupRoleId);
+        PagedResult<GroupRoleOutput> Search(GroupRoleSearchCriteria groupRoleSearchCriteria);
+        GroupRoleOutput Create(GroupRoleInput groupRoleInput);
+        GroupRoleOutput Update(long groupRoleId, GroupRoleInput groupRoleInput);
+        void Delete(long groupRoleId);
+    }
+}
