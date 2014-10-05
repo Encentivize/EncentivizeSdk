d6d630eb567fb2dfca5dcc0090029b79327949bb
Sereph <ryank@entelect.co.za>
2014-10-05 18:15:43 +0200

    Adding ability client

---
+++ src/Entelect.Encentivize.Sdk/MemberGrouping/Abilities/AbilityClient.cs
@@ -0,0 +1,26 @@
+﻿using Entelect.Encentivize.Sdk.GenericServices;
+using RestSharp;
+
+namespace Entelect.Encentivize.Sdk.MemberGrouping.Abilities
+{
+    public class AbilityClient : IAbilityClient
+    {
+        private readonly IEntityRetrievalService<AbilityOutput> _entityRetrievalService;
+        public AbilityClient(IRestClient restClient)
+        {
+            var entitySettings = new EntitySettings("Ability", "Abilities", "Abilities");
+            _entityRetrievalService = new EntityRetrievalService<AbilityOutput>(restClient, entitySettings);
+        }
+
+        public AbilityOutput Get(long achievementCategoryId)
+        {
+            return _entityRetrievalService.GetById(achievementCategoryId);
+        }
+
+        public PagedResult<AbilityOutput> Search(AbilitySearchCriteria achievementCategorySearchCriteria)
+        {
+            return _entityRetrievalService.FindBySearchCriteria(achievementCategorySearchCriteria);
+        }
+
+    }
+}
---
+++ src/Entelect.Encentivize.Sdk/MemberGrouping/Abilities/IAbilityClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Entelect.Encentivize.Sdk.MemberGrouping.Abilities
+{
+    public interface IAbilityClient
+    {
+        AbilityOutput Get(long achievementCategoryId);
+        PagedResult<AbilityOutput> Search(AbilitySearchCriteria achievementCategorySearchCriteria);
+    }
+}
