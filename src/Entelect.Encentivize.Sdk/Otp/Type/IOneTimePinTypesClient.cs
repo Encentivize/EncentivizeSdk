582e12e2bc4908840912fe064dac982514ef00f0
Ryan Kotzen <Ryank@entelect.co.za>
2014-10-06 09:12:08 +0200

    Adding otp types

---
+++ src/Entelect.Encentivize.Sdk/Otp/Type/IOneTimePinTypesClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Entelect.Encentivize.Sdk.Otp.Type
+{
+    public interface IOneTimePinTypesClient
+    {
+        OneTimePinTypeOutput Get(long oneTimePinTypeId);
+        PagedResult<OneTimePinTypeOutput> Search(OneTimePinTypeSearchCriteria oneTimePinTypeSearchCriteria);
+    }
+}
---
+++ src/Entelect.Encentivize.Sdk/Otp/Type/OneTimePinTypesClient.cs
@@ -0,0 +1,26 @@
+﻿using Entelect.Encentivize.Sdk.GenericServices;
+using RestSharp;
+
+namespace Entelect.Encentivize.Sdk.Otp.Type
+{
+    public class OneTimePinTypesClient : IOneTimePinTypesClient
+    {
+        private readonly IEntityRetrievalService<OneTimePinTypeOutput> _entityRetrievalService;
+        public OneTimePinTypesClient(IRestClient restClient)
+        {
+            var entitySettings = new EntitySettings("One Time Pin Type", "One Time Pin Types", "OneTimePinTypes");
+            _entityRetrievalService = new EntityRetrievalService<OneTimePinTypeOutput>(restClient, entitySettings);
+        }
+
+        public OneTimePinTypeOutput Get(long oneTimePinTypeId)
+        {
+            return _entityRetrievalService.GetById(oneTimePinTypeId);
+        }
+
+        public PagedResult<OneTimePinTypeOutput> Search(OneTimePinTypeSearchCriteria oneTimePinTypeSearchCriteria)
+        {
+            return _entityRetrievalService.FindBySearchCriteria(oneTimePinTypeSearchCriteria);
+        }
+
+    }
+}
