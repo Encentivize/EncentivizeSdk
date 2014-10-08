eadefc816c0e826d9860f1ac6c1765b45b84dd89
Ryan Kotzen <Ryank@entelect.co.za>
2014-10-08 08:31:26 +0200

    Tests for the OtpTypes api client

---
+++ test/Entelect.Encentivize.Sdk.IntegrationTests/OneTimePinTypeClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Entelect.Encentivize.Sdk.Otp.Configuration;
+using Entelect.Encentivize.Sdk.Otp.Type;
+using NUnit.Framework;
+
+namespace Entelect.Encentivize.Sdk.IntegrationTests
+{
+    public class OneTimePinTypesClientTests : SdkTestBase
+    {
+        [Test]
+        public void Search()
+        {
+            var searchResult = OneTimePinTypesClient.Search(new OneTimePinTypeSearchCriteria());
+            Assert.NotNull(searchResult);
+            Assert.Greater(searchResult.Data.Count, 0);
+        }
+
+        [Test]
+        public void GetById()
+        {
+            var item = OneTimePinTypesClient.Get(1);
+            Assert.NotNull(item);
+        }
+    }
+}
